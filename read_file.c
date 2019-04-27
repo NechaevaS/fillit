2a8f5a23c885580fbe7f2c68cbe51993068bad65
Svetlana Nechaeva <snechaev@student.42.us.org>
2019-04-26 19:12:43 -0700

    26.04.2

--- read_file.c
+++ read_file.c
@@ -6,7 +6,7 @@
 /*   By: snechaev <snechaev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 16:55:19 by snechaev          #+#    #+#             */
-/*   Updated: 2019/04/26 16:57:15 by snechaev         ###   ########.fr       */
+/*   Updated: 2019/04/26 19:10:19 by snechaev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdlib.h>
 #include "fillit.h"
 
-void from_board_to_pattern(pattern *p, board *b)
+void	from_board_to_pattern(pattern *p, board *b)
 {
 	int	i;
 	int	j;
@@ -22,10 +22,10 @@ void from_board_to_pattern(pattern *p, board *b)
 
 	j = 0;
 	pos = 0;
-	while(j < b->h)
+	while (j < b->h)
 	{
 		i = 0;
-		while(i < b->w)
+		while (i < b->w)
 		{
 			p->s[pos] = ELEM(b, i, j);
 			i++;
@@ -36,13 +36,14 @@ void from_board_to_pattern(pattern *p, board *b)
 		j++;
 	}
 }
-int fill_pattern(pattern *p)
+
+int		fill_pattern(pattern *p)
 {
 	board	*tmp;
-	int	x;
-	int	y;
-	int 	type;
-	int	i;
+	int		x;
+	int		y;
+	int		type;
+	int		i;
 
 	type = 0;
 	i = 0;
@@ -50,10 +51,10 @@ int fill_pattern(pattern *p)
 	while (type < 19)
 	{
 		x = 0;
-		while(x + g_t[type].w < 5)
+		while (x + g_t[type].w < 5)
 		{
 			y = 0;
-			while(y + g_t[type].h < 5)
+			while (y + g_t[type].h < 5)
 			{
 				place_tetr(tmp, g_t + type, x, y, '#');
 				from_board_to_pattern(p + i, tmp);
@@ -67,55 +68,52 @@ int fill_pattern(pattern *p)
 		type++;
 	}
 	free_board(tmp);
-	return(1);
+	return (1);
 }
 
-int get_id(pattern *p, char *str)
+int		get_id(pattern *p, char *str)
 {
 	int i;
-	
+
 	i = 0;
-	while(p[i].s[0])
+	while (p[i].s[0])
 	{
 		if (ft_strncmp(p[i].s, str, 20) == 0)
-			return(p[i].ntet);
+			return (p[i].ntet);
 		i++;
 	}
 	return (-1);
 }
 
-int   read_file(const int fd, tetr **all_read)
+int		read_file(const int fd, tetr **all_read)
 {
-	pattern	p[19 * 16];
-	char	buf[22];
-	int i;
-	int type;
-	int nread;
+	pattern		p[19 * 16];
+	char		buf[22];
+	int			i;
+	int			type;
+	int			nread;
 
 	ft_memset(p, 0, 19 * 16 * sizeof(pattern));
 	fill_pattern(p);
 	i = 0;
-	while (i < MAX_FIG)
+	while (i++ < MAX_FIG)
 	{
 		nread = read(fd, buf, 21);
 		buf[21] = '\0';
 		if ((type = get_id(p, buf)) == -1)
 		{
 			ft_putstr("error\n");
-			return(0);
+			return (0);
 		}
 		else
-		{
-			all_read[i] = g_t + get_id(p,buf);
-		}
+			all_read[i] = g_t + get_id(p, buf);
 		if (nread == 20)
 			return (1);
-		i++;
 	}
 	if (i == MAX_FIG)
 	{
 		ft_putstr("error\n");
-		return(0);
+		return (0);
 	}
 	return (1);
 }
--- solve.c
+++ solve.c
@@ -6,20 +6,19 @@
 /*   By: snechaev <snechaev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 14:20:38 by snechaev          #+#    #+#             */
-/*   Updated: 2019/04/26 18:08:26 by snechaev         ###   ########.fr       */
+/*   Updated: 2019/04/26 18:52:40 by snechaev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "fillit.h"
 
-int place_next(board *b, tetr **all_read, int i)
+int place_next(board *b, tetr **all_read, int ind)
 {
     int x;
     int y;
    
     y = 0;
-    i = 0;
     if (!all_read)
         return (1);
     while(y < b->h)
@@ -28,11 +27,13 @@ int place_next(board *b, tetr **all_read, int i)
         x = 0;
         while(x < b->w)
         {
-            if (place_tetr(b, *all_read, x, y, 'A' + i))
+            if (place_tetr(b, *all_read, x, y, 'A' + ind ))
             {   printBoard(b);
-                if (place_next(b, all_read + 1, i + 1)
+                if (place_next(b, all_read, ind + 1)
+                {
                 printf("%s\n", all_read);
                     return (1);
+                }
                 else
                     remove_tetr(b, *all_read, x, y);
             }
@@ -48,14 +49,15 @@ int solve(tetr **all_read)
     int i;
     int b_sz;
     board *b;
+
     i = 0;
     while(all_read[i] != NULL)
     {
         i++;
     }
-    b_sz = (i * 4)/2;
+    b_sz = (i * 4);
     b = create_board(b_sz);
-    while(!place_next(b, all_read, n))
+    while(!place_next(b, all_read, 0))
     {
         free_board(b);
         b_sz++;
